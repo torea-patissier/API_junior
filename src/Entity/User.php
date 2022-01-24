b50a1774a68f40b472c58368b00814b1c5cd00d2
Toréa Patissier <torea.patissier@laplateforme.io>
2022-01-24 14:38:56 +0100

    Token OK

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->email;
     }
 
+    public function getUsername(): ?string
+    {
+        return $this->email;
+    }
+
     public function setEmail(string $email): self
     {
         $this->email = $email;
