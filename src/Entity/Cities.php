7ec5b5e6ab6226494f8396cde420fb51a554e3e9
Toréa Patissier <torea.patissier@laplateforme.io>
2022-01-27 16:15:49 +0100

    PRBLM2

--- src/Entity/Cities.php
+++ src/Entity/Cities.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Annotation\Groups; // Pour la serialization et choisir les données
 use Doctrine\ORM\Mapping as ORM;
+use Proxies\__CG__\App\Entity\User;
+
 #[ORM\Entity(repositoryClass: CitiesRepository::class)]
 #[ApiResource]
 class Cities
