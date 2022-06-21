8682893a41c2f6399dc0028f1898288a6a128a9b
Toréa Patissier <torea.patissier@laplateforme.io>
2022-06-21 03:24:03 +0200

    image OFFV3

--- src/Serializer/UserPictureSerializer.php
+++ src/Serializer/UserPictureSerializer.php
@@ -10,37 +10,37 @@ use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 // use Vich\UploaderBundle\Storage\StorageInterface;
 use Symfony\Component\Security\Core\Security;
 
-// final class UserPictureSerializer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
-// {
-//   use NormalizerAwareTrait;
+final class UserPictureSerializer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
+{
+  use NormalizerAwareTrait;
 
-//   private const ALREADY_CALLED = 'USER_PICTURE_NORMALIZER_ALREADY_CALLED';
+  private const ALREADY_CALLED = 'USER_PICTURE_NORMALIZER_ALREADY_CALLED';
 
-//   public function __construct(private StorageInterface $storage, private EntityManagerInterface $em)
-//   {
-//   }
+  // public function __construct(private StorageInterface $storage, private EntityManagerInterface $em)
+  // {
+  // }
 
-//   public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
-//   {
-//     $context[self::ALREADY_CALLED] = true;
+  public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+  {
+    $context[self::ALREADY_CALLED] = true;
 
-//     if ($this->storage->resolveUri($object, 'photoFile')) {
-//       $object->setAvatar($this->storage->resolveUri($object, 'photoFile'));
-//     }
-
-
-//     $this->em->flush();
-
-//     return $this->normalizer->normalize($object, $format, $context);
-//   }
+    if ($this->storage->resolveUri($object, 'photoFile')) {
+      $object->setAvatar($this->storage->resolveUri($object, 'photoFile'));
+    }
 
 
-//   public function supportsNormalization($data, ?string $format = null, array $context = []): bool
-//   {
-//     if (isset($context[self::ALREADY_CALLED])) {
-//       return false;
-//     }
+    $this->em->flush();
 
-//     return $data instanceof User;
-//   }
-// }
+    return $this->normalizer->normalize($object, $format, $context);
+  }
+
+
+  public function supportsNormalization($data, ?string $format = null, array $context = []): bool
+  {
+    if (isset($context[self::ALREADY_CALLED])) {
+      return false;
+    }
+
+    return $data instanceof User;
+  }
+}
